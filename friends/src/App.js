07fd1fa47630dbba410eff962d2e34db510f054d
abarne <celius377@gmail.com>
2019-10-15 15:38:26 -0500

    Login functionality added

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
+import Login from './components/Login';
+import FriendsList from './components/FriendsList';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div className="App">
+				<Link to="/login">Login</Link>
+				<Link to="/protected">Friends List</Link>
+				<Switch>
+					<Route exact path="/protected" component={FriendsList} />
+					<Route path="/login" component={Login} />
+					<Route component={Login} />
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class FriendsList extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>FriendsList component!</h1>
+			</div>
+		);
+	}
+}
+
+export default FriendsList;
