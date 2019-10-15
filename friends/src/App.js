2d051c26745e5784db8ca1dbe127286aed6af882
abarne <celius377@gmail.com>
2019-10-15 15:48:43 -0500

    friends list is retrieved with axiosWithAuth and displayed

--- friends/src/App.js
+++ friends/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 import Login from './components/Login';
 import FriendsList from './components/FriendsList';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
 	return (
@@ -12,7 +13,7 @@ function App() {
 				<Link to="/login">Login</Link>
 				<Link to="/protected">Friends List</Link>
 				<Switch>
-					<Route exact path="/protected" component={FriendsList} />
+					<PrivateRoute exact path="/protected" component={FriendsList} />
 					<Route path="/login" component={Login} />
 					<Route component={Login} />
 				</Switch>
--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 class FriendsList extends React.Component {
+	state = {
+		friends: []
+	};
+
+	componentDidMount() {
+		this.getData();
+	}
+
+	getData = () => {
+		axiosWithAuth()
+			.get('/api/friends')
+			.then((response) => {
+				console.log(response);
+				this.setState({
+					friends: response.data
+				});
+			})
+			.catch((err) => console.log(err.response));
+	};
+
 	render() {
 		return (
 			<div>
-				<h1>FriendsList component!</h1>
+				<h1>Friends List Component</h1>
+				{this.state.friends.map((friend) => <h1>{friend.name}</h1>)}
 			</div>
 		);
 	}
